b6614e75589a0a57d0b1df72d2d6f4723f2761ac
robont112 <ernest.rodzik@student.pcez.pl>
2022-05-10 17:57:43 +0200

    animation

--- js/taktasd.js
+++ js/taktasd.js
@@ -1,2 +1,9 @@
-alert("HI ");
+nazwa = document.getElementById("nazwa");
+ console.log(nazwa);
 
+ nazwa.addEventListener("click" , animation);
+ 
+ function animation(e){
+     console.log(e.target);
+     nazwa.classList.add('animate__animated', 'animate__bounceOutLeft');
+    }
