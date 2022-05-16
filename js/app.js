7a9fb66603e2246f961f81463a30a6d96cd9dd18
robont112 <ernest.rodzik@student.pcez.pl>
2022-05-16 19:34:20 +0200

    game1

--- js/app.js
+++ js/app.js
@@ -1,104 +1,31 @@
-var startPoint = "x";
-var remis = 0;
-var wygrana=[];
-var p = [ 1, 2, 3, 4, 5];
-console.log(p[2]);
-table.addEventListener("click",clickField);
-function  clickField(e){
-  
-    if (e.target.innerHTML == ""){
-        remis++;
-
-    e.target.innerHTML = startPoint;
-    if (startPoint == "x"){
-        startPoint = "o";
-    } else{
-        startPoint = "x"
+var pierwiastki = [];
+var y = 0;
+container = document.getElementsByClassName("symbol");
+// console.log(container.length);
+for(x=0; x<container.length; x++){
+        if(container[x].textContent != "DE" 
+        &&container[x].textContent != "DEL" 
+        &&container[x].textContent != "57-71" 
+        && container[x].textContent != "89-103"){
+        pierwiastki[y] = container[x].textContent;
+        y++
     }
-    for(x=1 ; x< 10 ; x++ ){
-        p[x] = document.getElementById("p"+x).innerHTML;
-    }
-
-
-    }
-
-
-    function getElementP(elem,what) {
-        document.getElementById("p"+elem).innerHTML = '<div class="winColor">'+what+'</div>'
-    }
-
-    function getResult(info) {
-        document.getElementById("info").innerHTML = info ;
-    }
-
-    function wygranaGry(x,kto,par1,par2,par3) {
-        
-        wygrana[x]=p[par1] + p[par2] + p[par3];
-
-        console.log(wygrana[x]);
-            if(wygrana[x] == kto){
-
-                if (kto == "xxx"){
-
-                    getElementP(par1,"x");
-                    getElementP(par2,"x");
-                    getElementP(par3,"x");
-                    getResult("Wygrana Krzyżyków");
-                   table.removeEventListener("click",clickField);
-                }
-                if (kto == "ooo"){
-
-                    getElementP(par1,"o");
-                    getElementP(par2,"o");
-                    getElementP(par3,"o");
-                    getResult("Wygrana Kółeczków");
-                    table.removeEventListener("click",clickField);
-                }
-
-                resetButton.disabled = false;
-            }
-    }
-
-    wynik = [];
-    wynik[1] = 'xxx';
-    wynik[2] = 'ooo';
-
-    for(x=1; x<3; x++){
-        wygranaGry(1,wynik[x],1,2,3);
-        wygranaGry(2,wynik[x],4,5,6);
-        wygranaGry(3,wynik[x],7,8,9);
-        wygranaGry(4,wynik[x],1,4,7);
-        wygranaGry(5,wynik[x],2,5,8);
-        wygranaGry(6,wynik[x],3,6,9);
-        wygranaGry(7,wynik[x],1,5,9);
-        wygranaGry(8,wynik[x],4,5,7);
-    }
-
-    
-    if(remis == 9){
-        document.getElementById("resetButton").className = "visibleButton";
-    }
-    resetButton = document.getElementById("resetButton");
-    resetButton.addEventListener("click",resetButtonF)
-    function resetButtonF(e) {
-        table.addEventListener("click",clickField);
-        document.getElementById("p1").innerHTML = "";
-        document.getElementById("p2").innerHTML = "";
-        document.getElementById("p3").innerHTML = "";
-        document.getElementById("p4").innerHTML = "";
-        document.getElementById("p5").innerHTML = "";
-        document.getElementById("p6").innerHTML = "";
-        document.getElementById("p7").innerHTML = "";
-        document.getElementById("p8").innerHTML = "";
-        document.getElementById("p9").innerHTML = "";
-        document.getElementById("resetButton").className = "invisibleButton";
-        startPoint = "x";
-        remis = 0;
-    }
-    if(remis == 9){
-        document.getElementById("resetButton").className = "visibleButton";
-    }
-    
-    
-
 }
+for(x=0; x<pierwiastki.length; x++){
+    // console.log("x;" +x+ ","+pierwiastki[x]);
+}
+random = Math.floor(Math.random() * pierwiastki.length) ;
+console.log(pierwiastki[random]);
+
+for(x = 0 ;  x < container.length ;  x++){
+
+    if(container[x].textContent == pierwiastki[random])
+    wylosowany = container[x];
+}
+console.log(wylosowany)
+wylosowany = wylosowany.parentNode ;
+
+wylosowany.setAttribute("class","checked");
+console.log(wylosowany);
+
+ 
